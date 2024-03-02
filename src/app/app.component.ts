cf54003236a1c447b371c25b840d0cfd7a68f485
hussain1815 <hussainsajidpak@gmail.com>
2024-03-03 00:42:56 +0500

    card links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'profilecard';
+  constructor() { }
+
+  ngOnInit(): void {
+    // Test Bootstrap initialization
+    console.log('Bootstrap initialized');
+  }
 }
